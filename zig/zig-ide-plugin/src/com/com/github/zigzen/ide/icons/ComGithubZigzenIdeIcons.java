8f0a9f8648f3fc92c3db096aa14790e55147059f
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-02-15 23:12:08 +0800

    basic project creation

---
+++ zig/zig-ide-plugin/src/com/com/github/zigzen/ide/icons/ComGithubZigzenIdeIcons.java
@@ -0,0 +1,18 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.com.github.zigzen.ide.icons;
+
+import com.intellij.ui.IconManager;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+/**
+ * NOTE THIS FILE IS AUTO-GENERATED
+ * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
+ */
+public final class ComGithubZigzenIdeIcons {
+  private static @NotNull Icon load(@NotNull String path, int cacheKey, int flags) {
+    return IconManager.getInstance().loadRasterizedIcon(path, ComGithubZigzenIdeIcons.class.getClassLoader(), cacheKey, flags);
+  }
+  /** 16x16 */ public static final @NotNull Icon Zig = load("icons/zig.svg", 794600452, 0);
+}
