ff1490f04ea8620932e7c2587dc12351bc461f54
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-02-15 18:30:24 +0800

    add zig language and zig file type

---
+++ zig/zig-plugin/src/com/github/zigzen/openapi/ZigFileType.kt
@@ -0,0 +1,23 @@
+// Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.github.zigzen.openapi
+
+import com.github.zigzen.lang.ZigLanguage
+import com.intellij.openapi.fileTypes.LanguageFileType
+import javax.swing.Icon
+
+class ZigFileType : LanguageFileType(ZigLanguage.INSTANCE) {
+  companion object {
+    @JvmField
+    val INSTANCE = ZigFileType()
+  }
+
+  override fun getName(): String = "Zig File"
+
+  override fun getDescription(): String = "Zig file"
+
+  override fun getDefaultExtension(): String = "zig"
+
+  override fun getIcon(): Icon {
+    TODO("Not yet implemented")
+  }
+}
