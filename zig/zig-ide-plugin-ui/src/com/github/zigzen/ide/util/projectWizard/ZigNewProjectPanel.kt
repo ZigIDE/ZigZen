cc389b3f74ff016a204e627503ed720829ed1871
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-02-16 21:06:10 +0800

    add toolchain location thing

--- zig/zig-ide-plugin-ui/src/com/github/zigzen/ide/util/projectWizard/ZigNewProjectPanel.kt
+++ zig/zig-ide-plugin-ui/src/com/github/zigzen/ide/util/projectWizard/ZigNewProjectPanel.kt
@@ -1,12 +1,21 @@
 // Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.github.zigzen.ide.util.projectWizard
 
+import com.github.zigzen.ide.project.ui.ZigProjectSettingsPanel
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.util.Disposer
 import com.intellij.ui.dsl.builder.Panel
-import javax.swing.JLabel
 
-class ZigNewProjectPanel(private val updateListener: (() -> Unit)? = null) {
+class ZigNewProjectPanel(private val updateListener: (() -> Unit)? = null) : Disposable {
   val data: ZigNewProjectConfigurationData get() = ZigNewProjectConfigurationData(394)
 
+  private val zigProjectSettingsPanel = ZigProjectSettingsPanel()
+
+  override fun dispose() {
+    Disposer.dispose(zigProjectSettingsPanel)
+  }
+
   fun attachSelfTo(panel: Panel) = with(panel) {
+    zigProjectSettingsPanel.attachSelfTo(this)
   }
 }
