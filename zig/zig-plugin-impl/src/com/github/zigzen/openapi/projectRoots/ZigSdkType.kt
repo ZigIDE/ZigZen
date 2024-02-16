bd20691a1138af7c45ab6c1e785eebbcaf1759d9
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-02-16 15:07:51 +0800

    update

--- zig/zig-plugin-impl/src/com/github/zigzen/openapi/projectRoots/ZigSdkType.kt
+++ zig/zig-plugin-impl/src/com/github/zigzen/openapi/projectRoots/ZigSdkType.kt
@@ -11,12 +11,6 @@ import org.jdom.Element
 import javax.swing.Icon
 
 class ZigSdkType : SdkType("Zig SDK") {
-  override fun getIcon(): Icon = ZigZenIcons.Zig
-
-  override fun getPresentableName(): String {
-    TODO("Not yet implemented")
-  }
-
   override fun createAdditionalDataConfigurable(
     sdkModel: SdkModel,
     sdkModificator: SdkModificator
@@ -24,19 +18,19 @@ class ZigSdkType : SdkType("Zig SDK") {
     TODO("Not yet implemented")
   }
 
+  override fun getIcon(): Icon = ZigZenIcons.Zig
+
+  override fun getPresentableName(): String = name
+
   override fun isValidSdkHome(path: String): Boolean {
     TODO("Not yet implemented")
   }
 
-  override fun suggestHomePath(): String? {
-    TODO("Not yet implemented")
-  }
+  override fun suggestHomePath(): String? = null
 
   override fun suggestSdkName(currentSdkName: String?, sdkHome: String): String {
     TODO("Not yet implemented")
   }
 
-  override fun saveAdditionalData(additionalData: SdkAdditionalData, additional: Element) {
-    TODO("Not yet implemented")
-  }
+  override fun saveAdditionalData(additionalData: SdkAdditionalData, additional: Element) {}
 }
