5c423bbea248431d3daeb3208ebddfb1c38ce057
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-02-16 15:04:16 +0800

    use sdk instead

---
+++ zig/zig-plugin-impl/src/com/github/zigzen/openapi/projectRoots/ZigSdkType.kt
@@ -0,0 +1,42 @@
+// Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.github.zigzen.openapi.projectRoots
+
+import com.github.zigzen.icons.ZigZenIcons
+import com.intellij.openapi.projectRoots.AdditionalDataConfigurable
+import com.intellij.openapi.projectRoots.SdkAdditionalData
+import com.intellij.openapi.projectRoots.SdkModel
+import com.intellij.openapi.projectRoots.SdkModificator
+import com.intellij.openapi.projectRoots.SdkType
+import org.jdom.Element
+import javax.swing.Icon
+
+class ZigSdkType : SdkType("Zig SDK") {
+  override fun getIcon(): Icon = ZigZenIcons.Zig
+
+  override fun getPresentableName(): String {
+    TODO("Not yet implemented")
+  }
+
+  override fun createAdditionalDataConfigurable(
+    sdkModel: SdkModel,
+    sdkModificator: SdkModificator
+  ): AdditionalDataConfigurable? {
+    TODO("Not yet implemented")
+  }
+
+  override fun isValidSdkHome(path: String): Boolean {
+    TODO("Not yet implemented")
+  }
+
+  override fun suggestHomePath(): String? {
+    TODO("Not yet implemented")
+  }
+
+  override fun suggestSdkName(currentSdkName: String?, sdkHome: String): String {
+    TODO("Not yet implemented")
+  }
+
+  override fun saveAdditionalData(additionalData: SdkAdditionalData, additional: Element) {
+    TODO("Not yet implemented")
+  }
+}
--- zig/zig-plugin-impl/src/com/github/zigzen/toolchain/ZigDetectedToolchain.kt
+++
@@ -1,6 +0,0 @@
-// Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.github.zigzen.toolchain
-
-import com.github.zigzen.openapi.projectRoots.impl.ProjectToolchainImpl
-
-class ZigDetectedToolchain : ProjectToolchainImpl()
