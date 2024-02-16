7e292d5f56e1805adc23b7bf8c78a0d06e79abc4
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-02-16 22:40:12 +0800

    fix runtime errors

--- zig/zig-plugin-impl/src/javax/nio/file/PathExt.kt
+++ zig/zig-plugin-impl/src/javax/nio/file/PathExt.kt
@@ -1,7 +1,8 @@
 // Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
-package java.nio.file
+package javax.nio.file
 
 import com.intellij.openapi.util.SystemInfo
+import java.nio.file.Path
 
 fun Path.pathToBinary(name: String) : Path {
   val binaryName = if (SystemInfo.isWindows) "$name.exe" else name
