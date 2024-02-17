d7300804c9d22457cbc0ddaf232d9df52ad35772
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-02-17 20:47:56 +0800

    add ZON brace matching

---
+++ zig/zig-zon-plugin-impl/src/com/github/zigzen/lang/ZonBraceMatcher.kt
@@ -0,0 +1,18 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.github.zigzen.lang
+
+import com.github.zigzen.psi.ZonTypes
+import com.intellij.lang.BracePair
+import com.intellij.lang.PairedBraceMatcher
+import com.intellij.psi.PsiFile
+import com.intellij.psi.tree.IElementType
+
+class ZonBraceMatcher : PairedBraceMatcher {
+  override fun getPairs(): Array<BracePair> = arrayOf(
+    BracePair(ZonTypes.LBRACE, ZonTypes.RBRACE, true)
+  )
+
+  override fun isPairedBracesAllowedBeforeType(lbraceType: IElementType, contextType: IElementType?): Boolean = true
+
+  override fun getCodeConstructStart(file: PsiFile?, openingBraceOffset: Int): Int = openingBraceOffset
+}
