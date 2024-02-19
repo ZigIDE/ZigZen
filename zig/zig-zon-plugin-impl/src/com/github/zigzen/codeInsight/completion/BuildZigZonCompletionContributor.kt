e8baa57b6cd8f42c423aff00ffcd698e1655ed78
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-02-19 19:09:45 +0800

    add completion for dependencies field

--- zig/zig-zon-plugin-impl/src/com/github/zigzen/codeInsight/completion/BuildZigZonCompletionContributor.kt
+++ zig/zig-zon-plugin-impl/src/com/github/zigzen/codeInsight/completion/BuildZigZonCompletionContributor.kt
@@ -1,7 +1,9 @@
 // Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.github.zigzen.codeInsight.completion
 
-import com.github.zigzen.psi.ZonTypes
+import com.github.zigzen.extapi.psi.ZonPsiFile
+import com.github.zigzen.psi.ZonIncompleteStructProperty
+import com.github.zigzen.psi.ZonStructProperty
 import com.intellij.codeInsight.completion.CompletionContributor
 import com.intellij.codeInsight.completion.CompletionType
 import com.intellij.patterns.PlatformPatterns
@@ -11,9 +13,20 @@ class BuildZigZonCompletionContributor : CompletionContributor() {
     extend(
       CompletionType.BASIC,
       PlatformPatterns.psiElement()
-        .withParent(PlatformPatterns.psiElement(ZonTypes.INCOMPLETE_STRUCT_PROPERTY))
+        .withParent(ZonIncompleteStructProperty::class.java)
+        .withSuperParent(4, ZonPsiFile::class.java)
         .inVirtualFile(PlatformPatterns.virtualFile().withName("build.zig.zon")),
       BuildZigZonRootFieldCompletionProvider()
+    )
+
+    extend(
+      CompletionType.BASIC,
+      PlatformPatterns.psiElement()
+        .withParent(ZonIncompleteStructProperty::class.java)
+        .withSuperParent(4, ZonStructProperty::class.java)
+        .withSuperParent(7, ZonPsiFile::class.java)
+        .inVirtualFile(PlatformPatterns.virtualFile().withName("build.zig.zon")),
+      BuildZigZonDependencyFieldCompletionProvider()
     )
   }
 }
