9d9b7424a8bf7653ff9d97d452e3869e9c448577
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-02-19 18:13:11 +0800

    optimize patterns

--- zig/zig-zon-plugin-impl/src/com/github/zigzen/codeInsight/completion/BuildZigZonCompletionContributor.kt
+++ zig/zig-zon-plugin-impl/src/com/github/zigzen/codeInsight/completion/BuildZigZonCompletionContributor.kt
@@ -1,7 +1,6 @@
 // Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.github.zigzen.codeInsight.completion
 
-import com.github.zigzen.extapi.psi.ZonPsiFile
 import com.github.zigzen.psi.ZonTypes
 import com.intellij.codeInsight.completion.CompletionContributor
 import com.intellij.codeInsight.completion.CompletionType
@@ -13,7 +12,6 @@ class BuildZigZonCompletionContributor : CompletionContributor() {
       CompletionType.BASIC,
       PlatformPatterns.psiElement()
         .withParent(PlatformPatterns.psiElement(ZonTypes.INCOMPLETE_STRUCT_PROPERTY))
-        .withSuperParent(4, PlatformPatterns.psiElement(ZonPsiFile::class.java))
         .inVirtualFile(PlatformPatterns.virtualFile().withName("build.zig.zon")),
       BuildZigZonRootFieldCompletionProvider()
     )
