3628c43b758037dcb91cffcfce241bf3b0c7e193
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-02-29 17:58:05 +0800

    [zig] add documentation to completion popup for zon

---
+++ zig/zig-plugin-impl/src/com/github/zigzen/codeInsight/completion/ZigBuiltinFunctionCompletionContributor.kt
@@ -0,0 +1,19 @@
+// Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.github.zigzen.codeInsight.completion
+
+import com.github.zigzen.extapi.psi.ZigPsiFile
+import com.intellij.codeInsight.completion.CompletionContributor
+import com.intellij.codeInsight.completion.CompletionType
+import com.intellij.patterns.PlatformPatterns
+
+class ZigBuiltinFunctionCompletionContributor : CompletionContributor() {
+  init {
+    extend(
+      CompletionType.BASIC,
+      PlatformPatterns
+        .psiElement()
+        .inFile(PlatformPatterns.psiFile(ZigPsiFile::class.java)),
+      ZigBuiltinFunctionCompletionProvider(),
+    )
+  }
+}
