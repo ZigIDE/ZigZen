480500bc887ae5bf546ccb6e17d1509907b86074
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-02 20:23:24 +0800

    hyperlink in about popup

--- zig/zig-ide-plugin-ui/src/com/github/zigzen/ide/ZigZenAboutPopupDescriptionProvider.kt
+++ zig/zig-ide-plugin-ui/src/com/github/zigzen/ide/ZigZenAboutPopupDescriptionProvider.kt
@@ -4,5 +4,5 @@ package com.github.zigzen.ide
 import com.intellij.ide.AboutPopupDescriptionProvider
 
 class ZigZenAboutPopupDescriptionProvider : AboutPopupDescriptionProvider {
-  override fun getDescription(): String = "GitHub Repository: https://github.com/ZigIDE/ZigZen"
+  override fun getDescription(): String = "Source Code: <a href=\"https://github.com/ZigIDE/ZigZen\">GitHub</a>"
 }
