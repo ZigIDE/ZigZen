9c2ca1ef6a6fda22a17ddac313946616b4ce49c9
AnErrupTion <anerruption@disroot.org>
2024-03-02 09:11:17 +0100

    Re-organize Zig color settings page + add features
    Signed-off-by: AnErrupTion <anerruption@disroot.org>

---
+++ zig/zig-plugin-impl/src/com/github/zigzen/lang/ZigBraceMatcher.kt
@@ -0,0 +1,23 @@
+// Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.github.zigzen.lang
+
+import com.github.zigzen.lang.psi.ZigTypes
+import com.intellij.lang.BracePair
+import com.intellij.lang.PairedBraceMatcher
+import com.intellij.psi.PsiFile
+import com.intellij.psi.tree.IElementType
+
+class ZigBraceMatcher : PairedBraceMatcher {
+  @Suppress("CompanionObjectInExtension")
+  companion object {
+    val PAIRS = arrayOf(
+      BracePair(ZigTypes.LBRACE, ZigTypes.RBRACE, true)
+    )
+  }
+
+  override fun getPairs() = PAIRS
+
+  override fun isPairedBracesAllowedBeforeType(lbraceType: IElementType, contextType: IElementType?) = true
+
+  override fun getCodeConstructStart(file: PsiFile?, openingBraceOffset: Int) = openingBraceOffset
+}
