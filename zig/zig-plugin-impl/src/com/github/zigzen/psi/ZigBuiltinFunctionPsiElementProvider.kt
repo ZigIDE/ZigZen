544362b16a2b363ddf78fed122a8a9f2ec70a90e
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-03 17:41:30 +0800

    [zig] documentation for zig builtin functions

--- zig/zig-plugin-impl/src/com/github/zigzen/psi/ZigBuiltinFunctionPsiElementProvider.kt
+++ zig/zig-plugin-impl/src/com/github/zigzen/psi/ZigBuiltinFunctionPsiElementProvider.kt
@@ -1,7 +1,11 @@
 // Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.github.zigzen.psi
 
+import com.github.zigzen.lang.ZigLanguage
 import com.github.zigzen.openapi.ZigFileType
+import com.intellij.lang.documentation.DocumentationMarkup
+import com.intellij.lang.documentation.QuickDocHighlightingHelper
+import com.intellij.markdown.utils.doc.DocMarkdownToHtmlConverter
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiFileFactory
@@ -27,13 +31,30 @@ class ZigBuiltinFunctionPsiElementProvider(@NotNull project: Project) {
 
   fun getBuiltinFunctionNames(): List<String> = FN_PROTOS.mapNotNull { it.identifier?.text }
 
+  fun getBuiltinFunctionAsFnProtoByName(name: String): ZigFnProto? = FN_PROTOS.find { it.identifier?.text == name }
+
   fun getDocumentationForBuiltinFunction(name: String): String? {
     val sibling = FN_PROTOS.find { it.identifier?.text == name }?.parent?.parent?.prevSibling ?: return null
 
     if (sibling !is PsiComment) return null
     if (sibling.elementType != ZigTypes.DOC_COMMENT) return null
 
-    return sibling.text.replace("/// ", "")
+    val protoText = getBuiltinFunctionAsFnProtoByName(name)?.text ?: return null
+
+    return buildString {
+      append(DocumentationMarkup.DEFINITION_START)
+      append(QuickDocHighlightingHelper.getStyledCodeFragment(
+        sibling.project,
+        ZigLanguage.INSTANCE,
+        protoText.replace("fn ", "@")
+      ))
+      append(DocumentationMarkup.DEFINITION_END)
+      append(DocumentationMarkup.CONTENT_START)
+      append(
+        DocMarkdownToHtmlConverter.convert(sibling.project, sibling.text.replace("/// ", ""), ZigLanguage.INSTANCE)
+      )
+      append(DocumentationMarkup.CONTENT_END)
+    }
   }
 
   companion object {
