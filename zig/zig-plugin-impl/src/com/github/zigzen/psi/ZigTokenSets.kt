4344fc8cbdd26e93bc34c27cdfbb3a51f6e5c582
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-03 16:42:42 +0800

    add quote handler

--- zig/zig-plugin-impl/src/com/github/zigzen/psi/ZigTokenSets.kt
+++ zig/zig-plugin-impl/src/com/github/zigzen/psi/ZigTokenSets.kt
@@ -1,12 +1,11 @@
 // Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.github.zigzen.psi
 
-import com.github.zigzen.psi.ZigTypes
 import com.intellij.psi.tree.TokenSet
 
 interface ZigTokenSets {
   companion object {
     val COMMENTS = TokenSet.create(ZigTypes.LINE_COMMENT, ZigTypes.DOC_COMMENT, ZigTypes.CONTAINER_DOC_COMMENT)
-    val STRINGS = TokenSet.create(ZigTypes.STRING_LITERAL)
+    val STRINGS = TokenSet.create(ZigTypes.STRING_LITERAL, ZigTypes.STRING_LITERAL_MULTI)
   }
 }
