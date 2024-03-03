b98ff187303f5bcba15380a0ffe8ba508c582172
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-03 18:12:47 +0800

    [zig] attempt to add documentation for completion

--- zig/zig-plugin-impl/src/com/github/zigzen/psi/ZigBuiltinFunctionPsiElementProvider.kt
+++ zig/zig-plugin-impl/src/com/github/zigzen/psi/ZigBuiltinFunctionPsiElementProvider.kt
@@ -1,6 +1,7 @@
 // Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.github.zigzen.psi
 
+import com.github.zigzen.extapi.psi.ZigPsiFile
 import com.github.zigzen.lang.ZigLanguage
 import com.github.zigzen.openapi.ZigFileType
 import com.intellij.lang.documentation.DocumentationMarkup
@@ -8,12 +9,14 @@ import com.intellij.lang.documentation.QuickDocHighlightingHelper
 import com.intellij.markdown.utils.doc.DocMarkdownToHtmlConverter
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiComment
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFileFactory
+import com.intellij.psi.util.childrenOfType
 import com.intellij.psi.util.elementType
 import com.intellij.util.LocalTimeCounter
 import org.jetbrains.annotations.NotNull
 
-class ZigBuiltinFunctionPsiElementProvider(@NotNull project: Project) {
+class ZigBuiltinFunctionPsiElementProvider(@NotNull val project: Project) {
   private val BUILTINS_FILE = String(this.javaClass.getResourceAsStream("/language-helper/builtinFunctions.zig")!!.readAllBytes())
 
   private val PSI_FILE = PsiFileFactory.getInstance(project).createFileFromText(
@@ -32,6 +35,13 @@ class ZigBuiltinFunctionPsiElementProvider(@NotNull project: Project) {
   fun getBuiltinFunctionNames(): List<String> = FN_PROTOS.mapNotNull { it.identifier?.text }
 
   fun getBuiltinFunctionAsFnProtoByName(name: String): ZigFnProto? = FN_PROTOS.find { it.identifier?.text == name }
+
+  fun getBuiltinIdentifierByName(name: String): PsiElement? = createFile("const _ = @$name();")
+    .children[0]
+    .children[0]
+    .children[0]
+    .childrenOfType<ZigPrimaryTypeExpr>()[0]
+    .builtinidentifier
 
   fun getDocumentationForBuiltinFunction(name: String): String? {
     val sibling = FN_PROTOS.find { it.identifier?.text == name }?.parent?.parent?.prevSibling ?: return null
@@ -56,6 +66,15 @@ class ZigBuiltinFunctionPsiElementProvider(@NotNull project: Project) {
       append(DocumentationMarkup.CONTENT_END)
     }
   }
+
+  private fun createFile(text: String): ZigPsiFile = PsiFileFactory.getInstance(project).createFileFromText(
+    "psiElementFactory.zig",
+    ZigFileType.INSTANCE,
+    text,
+    LocalTimeCounter.currentTime(),
+    false,
+    true,
+  ) as ZigPsiFile
 
   companion object {
     fun createInstance(@NotNull project: Project) = ZigBuiltinFunctionPsiElementProvider(project)
