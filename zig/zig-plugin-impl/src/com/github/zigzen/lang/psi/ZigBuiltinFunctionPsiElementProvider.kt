635a42e8dd9a1109c8c926a529b2f369a35f2721
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-03 11:10:00 +0800

    [zig] figured psi-based completion out for builtin functions

--- zig/zig-plugin-impl/src/com/github/zigzen/lang/psi/ZigBuiltinFunctionPsiElementProvider.kt
+++ zig/zig-plugin-impl/src/com/github/zigzen/lang/psi/ZigBuiltinFunctionPsiElementProvider.kt
@@ -2,30 +2,32 @@
 package com.github.zigzen.lang.psi
 
 import com.github.zigzen.openapi.ZigFileType
-import com.intellij.ide.DataManager
-import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFileFactory
 import com.intellij.util.LocalTimeCounter
+import org.jetbrains.annotations.NotNull
 
-object ZigBuiltinFunctionPsiElementProvider {
+class ZigBuiltinFunctionPsiElementProvider(@NotNull project: Project) {
   private val BUILTINS_FILE = String(this.javaClass.getResourceAsStream("/language-helper/builtinFunctions.zig")!!.readAllBytes())
 
-  private val PSI_FILE =
-    PsiFileFactory.getInstance(CommonDataKeys.PROJECT.getData(DataManager.getInstance().dataContext)).createFileFromText(
-      "builtinFunctions.zig",
-      ZigFileType.INSTANCE,
-      BUILTINS_FILE,
-      LocalTimeCounter.currentTime(),
-      false,
-      true,
-    )
+  private val PSI_FILE = PsiFileFactory.getInstance(project).createFileFromText(
+    "builtinFunctions.zig",
+    ZigFileType.INSTANCE,
+    BUILTINS_FILE,
+    LocalTimeCounter.currentTime(),
+    false,
+    true,
+  )
 
-  private val FN_PROTOS by lazy {
-    PSI_FILE.children.filterIsInstance<ZigContainerDecl>().map { it.containerDeclAuto.containerDeclarationList }.flatten()
-      .mapNotNull { it.decl?.fnProto }
-  }
+  private val FN_PROTOS = PSI_FILE.children
+    .filterIsInstance<ZigContainerDeclaration>()
+    .mapNotNull { it.decl?.fnProto }
 
   fun getBuiltinFunctionNames(): List<String> = FN_PROTOS.mapNotNull { it.identifier?.text }
 
   fun getBuiltinFunctionAsFunctionProtoByName(name: String): ZigFnProto? = FN_PROTOS.find { it.identifier?.text == name }
+
+  companion object {
+    fun createInstance(@NotNull project: Project) = ZigBuiltinFunctionPsiElementProvider(project)
+  }
 }
