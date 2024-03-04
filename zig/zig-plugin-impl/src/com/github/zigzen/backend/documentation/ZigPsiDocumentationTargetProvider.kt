02a3e231b90a27de82a854b05a24c9532ad1e7d0
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-04 10:02:50 +0800

    fix builtin identifier completion element pattern

--- zig/zig-plugin-impl/src/com/github/zigzen/backend/documentation/ZigPsiDocumentationTargetProvider.kt
+++ zig/zig-plugin-impl/src/com/github/zigzen/backend/documentation/ZigPsiDocumentationTargetProvider.kt
@@ -1,12 +1,9 @@
 // Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.github.zigzen.backend.documentation
 
-import com.intellij.platform.backend.documentation.DocumentationTarget
 import com.intellij.platform.backend.documentation.PsiDocumentationTargetProvider
 import com.intellij.psi.PsiElement
 
 class ZigPsiDocumentationTargetProvider : PsiDocumentationTargetProvider {
-  override fun documentationTarget(element: PsiElement, originalElement: PsiElement?): DocumentationTarget {
-    return ZigDocumentationTarget(element, originalElement)
-  }
+  override fun documentationTarget(element: PsiElement, originalElement: PsiElement?) = ZigDocumentationTarget(element, originalElement)
 }
