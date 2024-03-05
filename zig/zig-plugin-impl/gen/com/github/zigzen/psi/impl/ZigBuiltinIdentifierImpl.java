07d9ecbbe0b4eea05c45fcb46934bf0300497b94
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-05 17:48:42 +0800

    [zig] add more documentation for builtin functions

---
+++ zig/zig-plugin-impl/gen/com/github/zigzen/psi/impl/ZigBuiltinIdentifierImpl.java
@@ -0,0 +1,36 @@
+// This is a generated file. Not intended for manual editing.
+package com.github.zigzen.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static com.github.zigzen.psi.ZigTypes.*;
+import com.intellij.extapi.psi.ASTWrapperPsiElement;
+import com.github.zigzen.psi.*;
+
+public class ZigBuiltinIdentifierImpl extends ASTWrapperPsiElement implements ZigBuiltinIdentifier {
+
+  public ZigBuiltinIdentifierImpl(@NotNull ASTNode node) {
+    super(node);
+  }
+
+  public void accept(@NotNull ZigVisitor visitor) {
+    visitor.visitBuiltinIdentifier(this);
+  }
+
+  @Override
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof ZigVisitor) accept((ZigVisitor)visitor);
+    else super.accept(visitor);
+  }
+
+  @Override
+  @NotNull
+  public PsiElement getBuiltinidentifier() {
+    return findNotNullChildByType(BUILTINIDENTIFIER);
+  }
+
+}
