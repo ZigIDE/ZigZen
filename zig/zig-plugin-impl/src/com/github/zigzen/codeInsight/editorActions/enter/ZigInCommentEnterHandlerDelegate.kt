9595ff76bb6056d0ab06db744a194fbbc7a65f9a
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-05 17:48:42 +0800

    add more documentation for builtin functions

---
+++ zig/zig-plugin-impl/src/com/github/zigzen/codeInsight/editorActions/enter/ZigInCommentEnterHandlerDelegate.kt
@@ -0,0 +1,67 @@
+// Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.github.zigzen.codeInsight.editorActions.enter
+
+import com.github.zigzen.extapi.psi.ZigPsiFile
+import com.github.zigzen.psi.ZigTypes
+import com.github.zigzen.psi.util.PsiUtil
+import com.intellij.codeInsight.editorActions.enter.EnterHandlerDelegate
+import com.intellij.codeInsight.editorActions.enter.EnterHandlerDelegateAdapter
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.actionSystem.EditorActionHandler
+import com.intellij.openapi.util.Ref
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiFile
+import com.intellij.psi.util.elementType
+import com.intellij.util.text.CharArrayUtil
+
+class ZigInCommentEnterHandlerDelegate : EnterHandlerDelegateAdapter() {
+  override fun preprocessEnter(
+    file: PsiFile,
+    editor: Editor,
+    caretOffset: Ref<Int>,
+    caretAdvance: Ref<Int>,
+    dataContext: DataContext,
+    originalHandler: EditorActionHandler?
+  ): EnterHandlerDelegate.Result {
+    if (file !is ZigPsiFile)
+      return EnterHandlerDelegate.Result.Continue
+
+    val document = editor.document
+    PsiDocumentManager.getInstance(file.project).commitDocument(document)
+
+    val caretOffsetDeref = caretOffset.get()
+    val chars = document.charsSequence
+
+    val offset = CharArrayUtil.shiftForward(chars, caretOffsetDeref, " \t")
+
+    val isAlreadyEol = offset < chars.length && chars[offset] == '\n'
+
+    var elementAtCaretOffset = file.findElementAt(offset) ?: return EnterHandlerDelegate.Result.Continue
+    if (isAlreadyEol && PsiUtil.isEolOrWhitespace(elementAtCaretOffset, offset)) {
+      elementAtCaretOffset = elementAtCaretOffset.prevSibling ?: return EnterHandlerDelegate.Result.Continue
+    }
+
+    if (elementAtCaretOffset.elementType != ZigTypes.DOC_COMMENT) {
+      return EnterHandlerDelegate.Result.Continue
+    }
+
+    if (chars.startsWith("///", offset)) {
+      val afterPrefixOffset = offset + 3
+      if (afterPrefixOffset < document.textLength && chars[afterPrefixOffset] != ' ')
+        document.insertString(afterPrefixOffset, " ")
+
+      caretOffset.set(offset)
+    } else {
+      val prefixToAdd = if (chars[caretOffsetDeref] != ' ') "/// " else "///"
+      document.insertString(caretOffsetDeref, prefixToAdd)
+      caretAdvance.set(prefixToAdd.length)
+    }
+
+    return EnterHandlerDelegate.Result.Default
+  }
+
+  override fun postProcessEnter(file: PsiFile, editor: Editor, dataContext: DataContext): EnterHandlerDelegate.Result {
+    return super.postProcessEnter(file, editor, dataContext)
+  }
+}
