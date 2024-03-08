70c414497c81e5de6ed18405733143a8688dd3b6
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-08 11:12:10 +0800

    [zig] fixed the docs finally

--- zig/zig-zon-plugin-impl/src/com/github/zigzen/backend/documentation/BuildZigZonPsiDocumentationTargetProvider.kt
+++ zig/zig-zon-plugin-impl/src/com/github/zigzen/backend/documentation/BuildZigZonPsiDocumentationTargetProvider.kt
@@ -1,10 +1,16 @@
 // Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.github.zigzen.backend.documentation
 
+import com.github.zigzen.lang.ZonLanguage
+import com.intellij.platform.backend.documentation.DocumentationTarget
 import com.intellij.platform.backend.documentation.PsiDocumentationTargetProvider
 import com.intellij.psi.PsiElement
 
 class BuildZigZonPsiDocumentationTargetProvider : PsiDocumentationTargetProvider {
-  override fun documentationTarget(element: PsiElement, originalElement: PsiElement?) =
-    BuildZigZonDocumentationTarget(element, originalElement)
+  override fun documentationTarget(element: PsiElement, originalElement: PsiElement?): DocumentationTarget? {
+    if (element.language.`is`(ZonLanguage.INSTANCE))
+      return BuildZigZonDocumentationTarget(element, originalElement)
+
+    return null
+  }
 }
