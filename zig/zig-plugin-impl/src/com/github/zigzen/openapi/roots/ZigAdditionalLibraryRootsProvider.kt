36b55a6a68b1648281880e197f35d3deba806103
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-08 16:58:34 +0800

    init zig std synthetic lib

---
+++ zig/zig-plugin-impl/src/com/github/zigzen/openapi/roots/ZigAdditionalLibraryRootsProvider.kt
@@ -0,0 +1,11 @@
+// Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.github.zigzen.openapi.roots
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.AdditionalLibraryRootsProvider
+
+class ZigAdditionalLibraryRootsProvider : AdditionalLibraryRootsProvider() {
+  override fun getAdditionalProjectLibraries(project: Project) = listOf(
+    ZigStandardLibrarySyntheticLibrary()
+  )
+}
