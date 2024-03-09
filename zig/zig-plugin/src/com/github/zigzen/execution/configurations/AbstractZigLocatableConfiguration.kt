d28ed27b895f0c62d33c946a9428c10368b29da2
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-09 22:05:14 +0800

    [zig] add more run config infra

--- zig/zig-plugin/src/com/github/zigzen/execution/configurations/AbstractZigLocatableConfiguration.kt
+++ zig/zig-plugin/src/com/github/zigzen/execution/configurations/AbstractZigLocatableConfiguration.kt
@@ -8,15 +8,35 @@ import com.intellij.execution.configurations.LocatableConfigurationBase
 import com.intellij.execution.configurations.RunProfileState
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.JDOMExternalizerUtil
+import com.intellij.openapi.util.io.toNioPathOrNull
+import org.jdom.Element
+import java.nio.file.Path
 
 abstract class AbstractZigLocatableConfiguration<T: AbstractZigLocatableConfiguration<T>>(
   project: Project,
   configurationFactory: ConfigurationFactory,
   name: String?,
 ) : LocatableConfigurationBase<AbstractZigCommandLineState<T>>(project, configurationFactory, name) {
-  override fun getConfigurationEditor() = ZigRunConfigurationsSettingsEditor(emptyList())
+  var workingDirectory: Path? = if (project.isDefault) { null } else { project.basePath?.toNioPathOrNull() }
+
+  override fun getConfigurationEditor() = ZigRunConfigurationsSettingsEditor(getConfigurationModules())
+
+  override fun readExternal(element: Element) {
+    super.readExternal(element)
+
+    workingDirectory = JDOMExternalizerUtil.readField(element, "WORKING_DIRECTORY")?.toNioPathOrNull()
+  }
+
+  override fun writeExternal(element: Element) {
+    super.writeExternal(element)
+
+    JDOMExternalizerUtil.writeField(element, "WORKING_DIRECTORY", workingDirectory?.toString())
+  }
 
   abstract override fun getState(executor: Executor, environment: ExecutionEnvironment): RunProfileState?
 
   abstract override fun suggestedName(): String
+
+  fun getConfigurationModules(): List<IZigLocatableConfigurationModule<T>> = listOf(ZigWorkingDirectoryLocatableConfigurationModule())
 }
