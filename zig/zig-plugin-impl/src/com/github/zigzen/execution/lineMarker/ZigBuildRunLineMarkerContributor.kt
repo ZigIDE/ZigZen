cb37f7510013373e9b7d4da97743102104a12e65
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-09 19:33:44 +0800

    [zig] add build line marker for build.zig

---
+++ zig/zig-plugin-impl/src/com/github/zigzen/execution/lineMarker/ZigBuildRunLineMarkerContributor.kt
@@ -0,0 +1,36 @@
+// Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.github.zigzen.execution.lineMarker
+
+import com.github.zigzen.lang.ZigLanguage
+import com.github.zigzen.psi.ZigFnProto
+import com.github.zigzen.psi.ZigTypes
+import com.intellij.execution.lineMarker.ExecutorAction
+import com.intellij.execution.lineMarker.RunLineMarkerContributor
+import com.intellij.icons.AllIcons
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.elementType
+
+class ZigBuildRunLineMarkerContributor : RunLineMarkerContributor() {
+  override fun getInfo(element: PsiElement): Info? {
+    if (!element.language.`is`(ZigLanguage.INSTANCE))
+      return null
+
+    if (element.elementType != ZigTypes.IDENTIFIER)
+      return null
+
+    if (!element.textMatches("build"))
+      return null
+
+    if (element.parent !is ZigFnProto)
+      return null
+
+    if ((element.containingFile?.virtualFile?.name ?: return null) != "build.zig")
+      return null
+
+    return Info(
+      AllIcons.RunConfigurations.TestState.Run,
+      ExecutorAction.getActions(0),
+      null
+    )
+  }
+}
