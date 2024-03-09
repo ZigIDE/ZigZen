01aad11b4114ec037e2eb4ad5108cab9d8e3dcaf
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-09 19:20:50 +0800

    [zig] add run line marker for zig test

--- zig/zig-plugin-impl/src/com/github/zigzen/execution/lineMarker/ZigRunRunLineMarkerContributor.kt
+++ zig/zig-plugin-impl/src/com/github/zigzen/execution/lineMarker/ZigRunRunLineMarkerContributor.kt
@@ -10,7 +10,7 @@ import com.intellij.icons.AllIcons
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.elementType
 
-class ZigRunLineMarkerContributor : RunLineMarkerContributor() {
+class ZigRunRunLineMarkerContributor : RunLineMarkerContributor() {
   override fun getInfo(element: PsiElement): Info? {
     if (!element.language.`is`(ZigLanguage.INSTANCE))
       return null
