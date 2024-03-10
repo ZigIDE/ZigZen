42cb759def7f1383c2b0e64f3db52ff1b4014b6a
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-10 14:38:57 +0800

    [zig] update classes

---
+++ zig/zig-plugin-impl/src/com/github/zigzen/execution/configurations/ZigBuildConfigurationFactory.kt
@@ -0,0 +1,10 @@
+// Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.github.zigzen.execution.configurations
+
+import com.intellij.execution.configurations.ConfigurationFactory
+import com.intellij.execution.configurations.ConfigurationType
+import com.intellij.openapi.project.Project
+
+class ZigBuildConfigurationFactory(configurationType: ConfigurationType) : ConfigurationFactory(configurationType) {
+  override fun createTemplateConfiguration(project: Project) = ZigBuildLocatableConfiguration(project, this)
+}
---
+++ zig/zig-plugin-impl/src/com/github/zigzen/execution/configurations/ZigBuildLocatableConfiguration.kt
@@ -0,0 +1,25 @@
+// Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.github.zigzen.execution.configurations
+
+import com.intellij.execution.Executor
+import com.intellij.execution.configurations.ConfigurationFactory
+import com.intellij.execution.configurations.RunProfileState
+import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.openapi.project.Project
+
+class ZigBuildLocatableConfiguration(
+  project: Project,
+  configurationFactory: ConfigurationFactory
+) : AbstractZigLocatableConfiguration<ZigBuildLocatableConfiguration>(project, configurationFactory, "Zig Build") {
+  override fun buildCommandLineArguments(): List<String> {
+    TODO("Not yet implemented")
+  }
+
+  override fun getState(executor: Executor, environment: ExecutionEnvironment): RunProfileState? {
+    TODO("Not yet implemented")
+  }
+
+  override fun suggestedName(): String {
+    TODO("Not yet implemented")
+  }
+}
