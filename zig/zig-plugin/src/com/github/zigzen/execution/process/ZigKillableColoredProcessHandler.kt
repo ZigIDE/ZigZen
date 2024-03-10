ce5f1f90bcc5de55a113a7cefbde5a950cce2281
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-10 19:23:18 +0800

    [zig] change impl of process handler

--- zig/zig-plugin/src/com/github/zigzen/execution/process/ZigKillableColoredProcessHandler.kt
+++ zig/zig-plugin/src/com/github/zigzen/execution/process/ZigKillableColoredProcessHandler.kt
@@ -4,12 +4,11 @@ package com.github.zigzen.execution.process
 import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.execution.configurations.PtyCommandLine
 import com.intellij.execution.process.AnsiEscapeDecoder
-import com.intellij.execution.process.KillableProcessHandler
-import com.intellij.openapi.util.Key
+import com.intellij.execution.process.KillableColoredProcessHandler
 import com.pty4j.PtyProcess
 import java.nio.charset.Charset
 
-class ZigKillableProcessHandler : KillableProcessHandler, AnsiEscapeDecoder.ColoredTextAcceptor {
+class ZigKillableColoredProcessHandler : KillableColoredProcessHandler, AnsiEscapeDecoder.ColoredTextAcceptor {
   constructor(commandLine: GeneralCommandLine) : super(commandLine) {
     setHasPty(commandLine is PtyCommandLine)
     setShouldDestroyProcessRecursively(!hasPty())
@@ -18,9 +17,5 @@ class ZigKillableProcessHandler : KillableProcessHandler, AnsiEscapeDecoder.Colo
   constructor(process: Process, commandLine: String, charset: Charset) : super(process,  commandLine, charset) {
     setHasPty(process is PtyProcess)
     setShouldDestroyProcessRecursively(!hasPty())
-  }
-
-  override fun coloredTextAvailable(text: String, attributes: Key<*>) {
-    TODO("Not yet implemented")
   }
 }
