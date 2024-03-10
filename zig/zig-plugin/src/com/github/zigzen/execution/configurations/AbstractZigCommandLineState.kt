e307462516d68a7ceff49b3004f145feeceda9e8
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-10 11:07:29 +0800

    [zig] move some classes

--- zig/zig-plugin/src/com/github/zigzen/execution/configurations/AbstractZigCommandLineState.kt
+++ zig/zig-plugin/src/com/github/zigzen/execution/configurations/AbstractZigCommandLineState.kt
@@ -2,14 +2,29 @@
 package com.github.zigzen.execution.configurations
 
 import com.github.zigzen.execution.process.ZigKillableProcessHandler
+import com.github.zigzen.openapi.components.ZigProjectSettingsService
 import com.intellij.execution.configurations.CommandLineState
 import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.openapi.components.service
 
 abstract class AbstractZigCommandLineState<T: AbstractZigLocatableConfiguration<T>>(
-  environment: ExecutionEnvironment
+  environment: ExecutionEnvironment,
+  private val configuration: T
 ) : CommandLineState(environment) {
   override fun startProcess() = ZigKillableProcessHandler(createGeneralCommandLine())
 
-  private fun createGeneralCommandLine(): GeneralCommandLine = TODO()
+  private fun createGeneralCommandLine(): GeneralCommandLine {
+    val service = environment.project.service<ZigProjectSettingsService>()
+    val toolchain = service.getToolchain()
+
+    val zigExecutablePath = toolchain?.pathToExecutable("zig")
+
+    return GeneralCommandLine()
+      .withExePath(zigExecutablePath.toString())
+      .withWorkDirectory(configuration.workingDirectory.toString())
+      .withCharset(Charsets.UTF_8)
+      .withRedirectErrorStream(true)
+      .withParameters(configuration.buildCommandLineArguments())
+  }
 }
--- zig/zig-plugin/src/com/github/zigzen/openapi/components/ZigProjectSettingsService.kt
+++ zig/zig-plugin/src/com/github/zigzen/openapi/components/ZigProjectSettingsService.kt
@@ -4,13 +4,14 @@ package com.github.zigzen.openapi.components
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.State
 import com.intellij.openapi.components.Storage
+import com.intellij.openapi.components.StoragePathMacros
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 
 @Service(Service.Level.PROJECT)
 @State(
   name = "ZigProjectSettings",
-  storages = [Storage("misc.xml")],
+  storages = [Storage(StoragePathMacros.WORKSPACE_FILE)],
 )
 class ZigProjectSettingsService(project: Project) : AbstractZigProjectSettingsService<ZigProjectSettings>(project, ZigProjectSettings()) {
   fun getToolchain() = state.getToolchain()
