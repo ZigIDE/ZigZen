12545f3f7208e44ee7e1bc76fc5fa1b96bc2b6a0
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-10 11:40:40 +0800

    add abstract config producer

--- zig/zig-plugin-impl/src/com/github/zigzen/backend/documentation/ZigDocumentationTargetProvider.kt
+++ zig/zig-plugin-impl/src/com/github/zigzen/backend/documentation/ZigDocumentationTargetProvider.kt
@@ -1,7 +1,7 @@
 // Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.github.zigzen.backend.documentation
 
-import com.github.zigzen.extapi.psi.ZigPsiFile
+import com.github.zigzen.psi.ZigPsiFile
 import com.intellij.platform.backend.documentation.DocumentationTarget
 import com.intellij.platform.backend.documentation.DocumentationTargetProvider
 import com.intellij.psi.PsiFile
