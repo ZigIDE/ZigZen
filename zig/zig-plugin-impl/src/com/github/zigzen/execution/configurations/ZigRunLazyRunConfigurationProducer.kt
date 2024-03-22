f61bc18901feae57f54298067701e57ffedb818e
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-22 12:57:11 +0800

    [zig] a plethora of run config related stuff

--- zig/zig-plugin-impl/src/com/github/zigzen/execution/configurations/ZigRunLazyRunConfigurationProducer.kt
+++ zig/zig-plugin-impl/src/com/github/zigzen/execution/configurations/ZigRunLazyRunConfigurationProducer.kt
@@ -1,9 +1,12 @@
 // Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.github.zigzen.execution.configurations
 
+import com.github.zigzen.execution.lineMarker.ZigRunRunLineMarkerContributor
+import com.intellij.execution.actions.ConfigurationFromContext
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
 import java.nio.file.Path
+import kotlin.io.path.invariantSeparatorsPathString
 
 class ZigRunLazyRunConfigurationProducer : AbstractZigLazyRunConfigurationProducer<ZigRunLocatableConfiguration>() {
   override fun getConfigurationFactory() = ZigRunConfigurationType.getInstance().configurationFactories[0]
@@ -16,11 +19,22 @@ class ZigRunLazyRunConfigurationProducer : AbstractZigLazyRunConfigurationProduc
     virtualFile: VirtualFile
   ): Boolean = true
 
-  // todo
   override fun setupConfigurationFromContextImpl(
     configuration: ZigRunLocatableConfiguration,
     element: PsiElement,
     path: Path,
     virtualFile: VirtualFile
-  ): Boolean = true
+  ): Boolean {
+    if (ZigRunRunLineMarkerContributor.instance.elementMatches(element)) {
+      configuration.setFilePath(path.invariantSeparatorsPathString)
+      configuration.name = virtualFile.presentableName
+      return true
+    }
+
+    return false
+  }
+
+  override fun shouldReplace(self: ConfigurationFromContext, other: ConfigurationFromContext): Boolean {
+    return self.configurationType is ZigRunConfigurationType
+  }
 }
--- zig/zig-plugin-impl/src/com/github/zigzen/execution/configurations/ZigRunLocatableConfiguration.kt
+++ zig/zig-plugin-impl/src/com/github/zigzen/execution/configurations/ZigRunLocatableConfiguration.kt
@@ -12,17 +12,20 @@ class ZigRunLocatableConfiguration(
   project: Project,
   configurationFactory: ConfigurationFactory
 ) : AbstractZigLocatableConfiguration<ZigRunLocatableConfiguration>(project, configurationFactory, "Zig Run"),
-    IZigColoredOutputLocatableConfigurationModuleConfigurationManipulator {
+    IZigColoredOutputLocatableConfigurationModuleConfigurationManipulator,
+    IZigFilePathLocatableConfigurationModuleConfigurationManipulator {
   private var colouredOutput = true
+  private var filePath = ""
 
   override fun buildCommandLineArguments(): MutableList<String> {
-    val currentArgs = mutableListOf("run", "--color", if (colouredOutput) { "on" } else { "off" })
+    val currentArgs = mutableListOf("run", "--color", if (colouredOutput) { "on" } else { "off" }, filePath)
     return currentArgs
   }
 
   override fun getConfigurationModules(): MutableList<IZigLocatableConfigurationModule<ZigRunLocatableConfiguration>> {
     val list = super.getConfigurationModules()
     list.add(ZigColoredOutputLocatableConfigurationModule())
+    list.add(ZigFilePathLocatableConfigurationModule())
 
     return list
   }
@@ -33,6 +36,7 @@ class ZigRunLocatableConfiguration(
     super.readExternal(element)
 
     colouredOutput = JDOMExternalizerUtil.readField(element, "COLOURED_OUTPUT").toBoolean()
+    filePath = JDOMExternalizerUtil.readField(element, "FILE_PATH").orEmpty()
   }
 
   override fun suggestedName(): String = "Run"
@@ -41,11 +45,18 @@ class ZigRunLocatableConfiguration(
     super.writeExternal(element)
 
     JDOMExternalizerUtil.writeField(element, "COLOURED_OUTPUT", colouredOutput.toString())
+    JDOMExternalizerUtil.writeField(element, "FILE_PATH", filePath)
   }
 
   override fun isColouredOutput() = colouredOutput
 
   override fun setColouredOutput(colouredOutput: Boolean) {
     this.colouredOutput = colouredOutput
+  }
+
+  override fun getFilePath(): String = filePath
+
+  override fun setFilePath(path: String) {
+    filePath = path
   }
 }
