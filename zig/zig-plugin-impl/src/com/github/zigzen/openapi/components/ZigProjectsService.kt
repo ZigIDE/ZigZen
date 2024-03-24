dbc25447b39d8f998c853b903bb90f06c6978252
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-24 10:57:51 +0800

    [zig] init zig project model

---
+++ zig/zig-plugin-impl/src/com/github/zigzen/openapi/components/ZigProjectsService.kt
@@ -0,0 +1,30 @@
+// Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.github.zigzen.openapi.components
+
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
+import com.intellij.openapi.components.StoragePathMacros
+import com.intellij.openapi.project.Project
+import org.jdom.Element
+
+@State(
+  name = "ZigProjectsService",
+  storages = [Storage(StoragePathMacros.WORKSPACE_FILE)]
+)
+class ZigProjectsService(
+  override val project: Project
+) : Disposable, IZigProjectsService, PersistentStateComponent<Element> {
+  override fun dispose() {
+    TODO("Not yet implemented")
+  }
+
+  override fun getState(): Element? {
+    TODO("Not yet implemented")
+  }
+
+  override fun loadState(state: Element) {
+    TODO("Not yet implemented")
+  }
+}
