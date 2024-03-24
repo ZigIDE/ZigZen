9c27e9e1ad9304dbf350909fcf735db8cbdb4389
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-24 10:57:51 +0800

    [zig] init zig project model

---
+++ zig/zig-plugin/src/com/github/zigzen/openapi/components/IZigProjectsService.kt
@@ -0,0 +1,8 @@
+// Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.github.zigzen.openapi.components
+
+import com.intellij.openapi.project.Project
+
+interface IZigProjectsService {
+  val project: Project
+}
