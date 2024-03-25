58ae23a40b64ccabdbcef415fefcd4ff2e67ad23
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-25 12:27:46 +0800

    [zig] add task

---
+++ zig/zig-plugin-impl/src/com/github/zigzen/openapi/progress/ZigSynchronizationTask.kt
@@ -0,0 +1,58 @@
+// Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.github.zigzen.openapi.progress
+
+import com.github.zigzen.projectModel.ZigProject
+import com.intellij.build.BuildContentDescriptor
+import com.intellij.build.SyncViewManager
+import com.intellij.build.progress.BuildProgress
+import com.intellij.build.progress.BuildProgressDescriptor
+import com.intellij.openapi.progress.ProcessCanceledException
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.Task
+import com.intellij.openapi.project.Project
+import java.util.concurrent.CompletableFuture
+import javax.swing.JComponent
+
+class ZigSynchronizationTask(
+  project: Project,
+  private val zigProjects: Collection<ZigProject>,
+  private val result: CompletableFuture<Collection<ZigProject>>
+) : Task.Backgroundable(project, "Reloading Zig projects", true) {
+  override fun run(indicator: ProgressIndicator) {
+    indicator.isIndeterminate = true
+
+    val buildProgress = SyncViewManager.createBuildProgress(project)
+
+    val refreshedProjects = try {
+      buildProgress.start(createProjectSynchronizationDescriptor(indicator))
+      val refreshedProjects = performSynchronization(indicator, buildProgress)
+
+      refreshedProjects
+    } catch(e: Throwable) {
+      if (e is ProcessCanceledException)
+        buildProgress.cancel()
+      else
+        buildProgress.fail()
+
+      result.completeExceptionally(e)
+      throw e
+    }
+
+    result.complete(refreshedProjects)
+  }
+
+  private fun createProjectSynchronizationDescriptor(progressIndicator: ProgressIndicator): BuildProgressDescriptor {
+    val buildContentDescriptor = BuildContentDescriptor(null, null, object : JComponent() {}, "Zig")
+    buildContentDescriptor.isActivateToolWindowWhenFailed = true
+    buildContentDescriptor.isActivateToolWindowWhenAdded = false
+
+    TODO()
+  }
+
+  private fun performSynchronization(
+    indicator: ProgressIndicator,
+    progress: BuildProgress<BuildProgressDescriptor>
+  ): Collection<ZigProject> {
+    TODO()
+  }
+}
