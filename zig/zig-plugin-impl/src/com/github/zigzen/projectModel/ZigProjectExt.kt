07bc974f93a2b7e18eff4f3e55a15a01248e8291
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-25 11:24:58 +0800

    [zig] some stuff

---
+++ zig/zig-plugin-impl/src/com/github/zigzen/projectModel/ZigProjectExt.kt
@@ -0,0 +1,29 @@
+// Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.github.zigzen.projectModel
+
+import com.intellij.ide.impl.isTrusted
+import com.intellij.openapi.project.Project
+import java.nio.file.Path
+import java.util.concurrent.CompletableFuture
+
+fun Collection<ZigProject>.isExistingProject(buildZigZon: Path): Boolean {
+  if (any { it.buildZigZon == buildZigZon })
+    return true
+
+  return any { it.rootDir == buildZigZon.parent }
+}
+
+fun Collection<ZigProject>.refreshProjects(project: Project): CompletableFuture<Collection<ZigProject>> {
+  if (!project.isTrusted())
+    return CompletableFuture.completedFuture(this)
+
+  return if (isEmpty())
+    CompletableFuture.completedFuture(emptyList())
+  else {
+    val future = CompletableFuture<Collection<ZigProject>>()
+
+    future
+  }.thenApply { updatedProjects ->
+    updatedProjects
+  }
+}
