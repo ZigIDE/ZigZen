cee9a6745c91801ec69bbd11a1fe785a76ca739e
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-25 11:24:58 +0800

    [zig] some stuff

--- zig/zig-plugin/src/com/github/zigzen/openapi/components/IZigProjectsService.kt
+++ zig/zig-plugin/src/com/github/zigzen/openapi/components/IZigProjectsService.kt
@@ -1,10 +1,13 @@
 // Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.github.zigzen.openapi.components
 
+import com.github.zigzen.projectModel.IZigProject
 import com.intellij.openapi.project.Project
+import java.nio.file.Path
 
 interface IZigProjectsService {
   val project: Project
+  val allProjects: Collection<IZigProject>
 
-  fun attachZigProject(): Boolean
+  fun attachZigProject(buildZigZon: Path): Boolean
 }
