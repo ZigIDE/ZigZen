ca374741901fe3718efaef1c5c7cc0ff66315bbc
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-27 23:47:23 +0800

    add handle

---
+++ zig/zig-plugin/src/javax/lang/ThrowableExt.kt
@@ -0,0 +1,12 @@
+// Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
+package javax.lang
+
+import com.github.zigzen.openapi.components.IZigProjectsRefreshListener
+import com.intellij.openapi.progress.ProcessCanceledException
+
+fun Throwable.toZigProjectsRefreshStatus(): IZigProjectsRefreshListener.ZigProjectsRefreshStatus {
+  return when {
+    this is ProcessCanceledException -> IZigProjectsRefreshListener.ZigProjectsRefreshStatus.CANCELLED
+    else -> IZigProjectsRefreshListener.ZigProjectsRefreshStatus.FAILURE
+  }
+}
