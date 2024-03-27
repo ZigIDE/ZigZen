4efcad1d8cb0a69dc7ba8e7dd8009c67795ba784
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-27 23:10:51 +0800

    [zig] change file type as an object

--- zig/zig-plugin-impl/src/com/github/zigzen/openapi/components/ZigProjectsService.kt
+++ zig/zig-plugin-impl/src/com/github/zigzen/openapi/components/ZigProjectsService.kt
@@ -1,21 +1,30 @@
 // Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.github.zigzen.openapi.components
 
+import com.github.zigzen.openapi.ZigFileType
 import com.github.zigzen.projectModel.ZigProject
 import com.github.zigzen.projectModel.isExistingProject
 import com.github.zigzen.projectModel.refreshProject
 import com.github.zigzen.util.concurrency.AsyncValue
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.invokeAndWaitIfNeeded
+import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.components.PersistentStateComponent
 import com.intellij.openapi.components.State
 import com.intellij.openapi.components.Storage
 import com.intellij.openapi.components.StoragePathMacros
+import com.intellij.openapi.fileTypes.FileTypeManager
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.RootsChangeRescanningInfo
+import com.intellij.openapi.project.ex.ProjectEx
+import com.intellij.openapi.roots.ex.ProjectRootManagerEx
+import com.intellij.openapi.util.EmptyRunnable
 import org.jdom.Element
 import java.nio.file.Path
 import java.util.concurrent.CompletableFuture
 import kotlin.io.path.invariantSeparatorsPathString
 
+@Suppress("TestOnlyProblems")
 @State(
   name = "ZigProjectsService",
   storages = [Storage(StoragePathMacros.WORKSPACE_FILE)]
@@ -23,7 +32,7 @@ import kotlin.io.path.invariantSeparatorsPathString
 class ZigProjectsService(
   override val project: Project
 ) : Disposable, IZigProjectsService, PersistentStateComponent<Element> {
-  private val projects = AsyncValue<List<ZigProject>>(emptyList())
+  private val projects = AsyncValue<Collection<ZigProject>>(emptyList())
 
   override val allProjects: Collection<ZigProject>
     get() = projects.currentValue
@@ -65,6 +74,30 @@ class ZigProjectsService(
   private fun modifyZigProjects(
     updater: (Collection<ZigProject>) -> CompletableFuture<Collection<ZigProject>>
   ): CompletableFuture<Collection<ZigProject>> {
-    TODO()
+    val refreshStatusPublisher = project.messageBus.syncPublisher(IZigProjectsService.cargoProjectsRefreshTopic)
+
+    val wrappedUpdater = { projects: Collection<ZigProject> ->
+      refreshStatusPublisher.onRefreshStarted()
+      updater(projects)
+    }
+
+    return projects.updateAsync(wrappedUpdater)
+      .thenApply { projects ->
+        invokeAndWaitIfNeeded {
+          val fileTypeManager = FileTypeManager.getInstance()
+          runWriteAction {
+            if (projects.isNotEmpty()) {
+              fileTypeManager.associateExtension(ZigFileType, ZigFileType.defaultExtension)
+            }
+
+            if ((project as? ProjectEx)?.isLight != true) {
+              ProjectRootManagerEx.getInstanceEx(project)
+                .makeRootsChange(EmptyRunnable.INSTANCE, false, true)
+            }
+          }
+        }
+
+        projects
+      }
   }
 }
