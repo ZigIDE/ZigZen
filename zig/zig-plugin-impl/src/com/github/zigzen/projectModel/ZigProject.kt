d02ed4caa4ec50e24239d7d6bb621d1ce2dd0c96
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-28 10:53:04 +0800

    [zig] update project service

--- zig/zig-plugin-impl/src/com/github/zigzen/projectModel/ZigProject.kt
+++ zig/zig-plugin-impl/src/com/github/zigzen/projectModel/ZigProject.kt
@@ -1,20 +1,19 @@
 // Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.github.zigzen.projectModel
 
-import com.intellij.openapi.project.Project
+import com.github.zigzen.openapi.components.ZigProjectsService
 import com.intellij.openapi.util.Key
 import com.intellij.openapi.vfs.VirtualFile
 import java.nio.file.Path
 
-class ZigProject : IZigProject {
-  override val buildZigZon: Path
-    get() = TODO("Not yet implemented")
-
+data class ZigProject(
+  override val buildZigZon: Path,
+  private val projectService: ZigProjectsService
+) : IZigProject {
   override val presentableName: String
     get() = TODO("Not yet implemented")
 
-  override val project: Project
-    get() = TODO("Not yet implemented")
+  override val project = projectService.project
 
   override val rootDir: VirtualFile
     get() = TODO("Not yet implemented")
