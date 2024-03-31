0e2ce75c27f4a1912b40a3a21000cdcf65b73e04
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-03-31 16:43:44 +0800

    [zig] add root dir and presentable name

--- zig/zig-plugin-impl/src/com/github/zigzen/projectModel/ZigProject.kt
+++ zig/zig-plugin-impl/src/com/github/zigzen/projectModel/ZigProject.kt
@@ -3,18 +3,30 @@ package com.github.zigzen.projectModel
 
 import com.github.zigzen.openapi.components.ZigProjectsService
 import com.intellij.openapi.util.UserDataHolderBase
+import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VirtualFile
 import java.nio.file.Path
+import java.util.concurrent.atomic.AtomicReference
 
 data class ZigProject(
   override val buildZigZon: Path,
   private val projectService: ZigProjectsService
 ) : IZigProject, UserDataHolderBase() {
-  override val presentableName: String
-    get() = TODO("Not yet implemented")
+  override val presentableName = buildZigZon.parent.fileName.toString()
 
   override val project = projectService.project
 
-  override val rootDir: VirtualFile
-    get() = TODO("Not yet implemented")
+  override val rootDir: VirtualFile?
+    get() {
+      val cached = cachedRootDir.get()
+      if (cached != null && cached.isValid)
+        return cached
+
+      val file = LocalFileSystem.getInstance().findFileByIoFile(buildZigZon.parent.toFile())
+      cachedRootDir.set(file)
+
+      return file
+    }
+
+  private val cachedRootDir = AtomicReference<VirtualFile>()
 }
