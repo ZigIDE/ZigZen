1a8480a80096d1e928c5b0583df4d3683c14e113
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-04-03 17:24:08 +0800

    [zig] add more code for project service

--- zig/zig-plugin-impl/src/com/github/zigzen/projectModel/ZigProjectExt.kt
+++ zig/zig-plugin-impl/src/com/github/zigzen/projectModel/ZigProjectExt.kt
@@ -1,8 +1,18 @@
 // Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.github.zigzen.projectModel
 
+import com.github.zigzen.openapi.progress.ZigSynchronizationTask
+import com.intellij.execution.RunManager
 import com.intellij.ide.impl.isTrusted
+import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.ex.ProjectEx
+import com.intellij.openapi.project.taskQueue
+import com.intellij.openapi.project.zigProjects
+import com.intellij.openapi.roots.ProjectFileIndex
+import com.intellij.openapi.roots.ex.ProjectRootManagerEx
+import com.intellij.openapi.vfs.setupContentRoots
+import com.intellij.util.ui.EdtInvocationManager.invokeAndWaitIfNeeded
 import java.nio.file.Path
 import java.util.concurrent.CompletableFuture
 
@@ -21,9 +31,35 @@ fun Collection<ZigProject>.refreshProject(project: Project): CompletableFuture<C
     CompletableFuture.completedFuture(emptyList())
   else {
     val future = CompletableFuture<Collection<ZigProject>>()
+    val synchronizationTask = ZigSynchronizationTask(project, this, future)
 
+    project.taskQueue.run(synchronizationTask)
     future
   }.thenApply { updatedProjects ->
+    if ((project as? ProjectEx)?.isLight != true) {
+      setupProjectRoots(project)
+    }
+
     updatedProjects
+  }
+}
+
+fun setupProjectRoots(project: Project) {
+  invokeAndWaitIfNeeded {
+    RunManager.getInstance(project)
+    ProjectFileIndex.getInstance(project)
+
+    runWriteAction {
+      if (project.isDisposed) return@runWriteAction
+      ProjectRootManagerEx.getInstanceEx(project).mergeRootsChangesDuring {
+        project.zigProjects.allProjects.forEach {
+          it.rootDir?.setupContentRoots(it.project) {
+            addExcludeFolder("zig-cache")
+            addExcludeFolder("zig-out")
+            addSourceFolder("src", false)
+          }
+        }
+      }
+    }
   }
 }
--- zig/zig-plugin-impl/src/com/intellij/openapi/project/ProjectExt.kt
+++ zig/zig-plugin-impl/src/com/intellij/openapi/project/ProjectExt.kt
@@ -1,9 +1,11 @@
 // Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.project
 
+import com.github.zigzen.openapi.components.ZigProjectTaskQueueService
 import com.intellij.ide.IdeBundle
 import com.intellij.ide.impl.isTrusted
 import com.intellij.ide.trustedProjects.TrustedProjectsDialog
+import com.intellij.openapi.components.service
 
 fun Project.confirmLoadingUntrustedProject(): Boolean {
   return isTrusted() || TrustedProjectsDialog.confirmLoadingUntrustedProject(
@@ -14,3 +16,5 @@ fun Project.confirmLoadingUntrustedProject(): Boolean {
     IdeBundle.message("untrusted.project.dialog.distrust.button"),
   )
 }
+
+val Project.taskQueue: ZigProjectTaskQueueService get() = service()
