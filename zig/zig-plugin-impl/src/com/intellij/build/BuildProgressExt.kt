5d884807ff1e8fd8e4b9030be3511d02f9a015f4
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-04-04 12:38:41 +0800

    [zig] add stdlib

---
+++ zig/zig-plugin-impl/src/com/intellij/build/BuildProgressExt.kt
@@ -0,0 +1,29 @@
+// Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.build
+
+import com.intellij.build.events.BuildEventsNls
+import com.intellij.build.progress.BuildProgress
+import com.intellij.build.progress.BuildProgressDescriptor
+import com.intellij.openapi.progress.ProcessCanceledException
+
+fun <T, R> BuildProgress<BuildProgressDescriptor>.runWithChildProgress(
+  @BuildEventsNls.Title title: String,
+  createContext: (BuildProgress<BuildProgressDescriptor>) -> T,
+  action: (T) -> R,
+  onResult: (BuildProgress<BuildProgressDescriptor>, R) -> Unit = { progress, _ -> progress.finish() }
+): R {
+  val childProgress = startChildProgress(title)
+  try {
+    val context = createContext(childProgress)
+    val result = action(context)
+    onResult(childProgress, result)
+    return result
+  } catch (e: Throwable) {
+    if (e is ProcessCanceledException) {
+      cancel()
+    } else {
+      fail()
+    }
+    throw e
+  }
+}
