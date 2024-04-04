4aab71f21a5c01e5183b847f71f87fb2166a821c
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-04-04 12:38:41 +0800

    add stdlib

--- zig/zig-plugin/src/com/github/zigzen/projectModel/IZigProject.kt
+++ zig/zig-plugin/src/com/github/zigzen/projectModel/IZigProject.kt
@@ -2,6 +2,7 @@
 package com.github.zigzen.projectModel
 
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.NlsContexts
 import com.intellij.openapi.util.UserDataHolderEx
 import com.intellij.openapi.vfs.VirtualFile
 import java.nio.file.Path
@@ -12,4 +13,14 @@ interface IZigProject : UserDataHolderEx {
   val rootDir: VirtualFile?
 
   val presentableName: String
+
+  val stdlibStatus: ProjectUpdateStatus
+
+  sealed class ProjectUpdateStatus(private val priority: Int) {
+    data object UpToDate : ProjectUpdateStatus(0)
+    data object NeedsUpdate : ProjectUpdateStatus(1)
+    class UpdateFailed(@Suppress("UnstableApiUsage") @NlsContexts.Tooltip val reason: String) : ProjectUpdateStatus(2) {
+      override fun toString(): String = reason
+    }
+  }
 }
