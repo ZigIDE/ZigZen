beff8975816b1972a102858a1d96d7e5970a02b5
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-04-07 00:00:49 +0800

    [zig] add run with child progress for sync task

--- zig/zig-plugin-impl/src/com/intellij/build/BuildProgressExt.kt
+++ zig/zig-plugin-impl/src/com/intellij/build/BuildProgressExt.kt
@@ -2,6 +2,7 @@
 package com.intellij.build
 
 import com.intellij.build.events.BuildEventsNls
+import com.intellij.build.events.MessageEvent
 import com.intellij.build.progress.BuildProgress
 import com.intellij.build.progress.BuildProgressDescriptor
 import com.intellij.openapi.progress.ProcessCanceledException
@@ -22,6 +23,29 @@ fun <T, R> BuildProgress<BuildProgressDescriptor>.runWithChildProgress(
     if (e is ProcessCanceledException) {
       cancel()
     } else {
+      fail()
+    }
+    throw e
+  }
+}
+
+fun <T, R> BuildProgress<BuildProgressDescriptor>.runWithChildProgressCatchingException(
+  @BuildEventsNls.Title title: String,
+  createContext: (BuildProgress<BuildProgressDescriptor>) -> T,
+  action: (T) -> R,
+  onResult: (BuildProgress<BuildProgressDescriptor>, R) -> Unit = { progress, _ -> progress.finish() }
+): R {
+  val childProgress = startChildProgress(title)
+  try {
+    val context = createContext(childProgress)
+    val result = action(context)
+    onResult(childProgress, result)
+    return result
+  } catch (e: Throwable) {
+    if (e is ProcessCanceledException) {
+      cancel()
+    } else {
+      childProgress.message("Process Exception", e.message.orEmpty(), MessageEvent.Kind.ERROR, null)
       fail()
     }
     throw e
