26865e3bcde85cb6b05d1d431839655ee1b8a3b3
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-04-08 21:16:11 +0800

    [zig] add workspace stuff

---
+++ zig/zig-plugin/src/zigzen/projectModel/IZigWorkspace.kt
@@ -0,0 +1,38 @@
+// Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
+package zigzen.projectModel
+
+import com.intellij.openapi.util.UserDataHolderEx
+import com.intellij.openapi.vfs.VirtualFile
+import java.nio.file.Path
+
+interface IZigWorkspace {
+  val buildZig: Path
+  val contentRoot: Path
+    get() = buildZig.parent
+
+  val root: VirtualFile?
+
+  val packages: Collection<IZigPackage>
+
+  fun withStandardLibrary(stdlib: ZigStandardLibrary): IZigWorkspace
+
+  interface IZigPackage : UserDataHolderEx {
+    val contentRoot: VirtualFile?
+    val rootDirectory: Path
+    val dependencies: Collection<IZigDependency>
+
+    val name: String
+  }
+
+  interface IZigDependency {
+    val `package`: IZigPackage
+    val dependencyKind: ZigDependencyKind
+
+    val name: String
+
+    enum class ZigDependencyKind {
+      StandardLibrary,
+      Unclassified,
+    }
+  }
+}
