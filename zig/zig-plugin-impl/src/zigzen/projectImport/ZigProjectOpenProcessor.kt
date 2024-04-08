219b1b506a4724c4e592343ee7675b60f9b4168a
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-04-08 18:23:14 +0800

    [zig] fix kotlinx serialization

--- zig/zig-plugin-impl/src/zigzen/projectImport/ZigProjectOpenProcessor.kt
+++ zig/zig-plugin-impl/src/zigzen/projectImport/ZigProjectOpenProcessor.kt
@@ -2,13 +2,11 @@
 package zigzen.projectImport
 
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.startup.StartupManager
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.PlatformProjectOpenProcessor
 import com.intellij.projectImport.ProjectOpenProcessor
 import zigzen.icons.ZigZenIcons
-import zigzen.openapi.components.ZigProjectsService
 
 class ZigProjectOpenProcessor : ProjectOpenProcessor() {
   override val icon = ZigZenIcons.Zig
@@ -23,10 +21,6 @@ class ZigProjectOpenProcessor : ProjectOpenProcessor() {
   override fun doOpenProject(virtualFile: VirtualFile, projectToClose: Project?, forceOpenInNewFrame: Boolean): Project? {
     val rootDirectory = if (virtualFile.isDirectory) virtualFile else virtualFile.parent
 
-    return PlatformProjectOpenProcessor.getInstance().doOpenProject(rootDirectory, projectToClose, forceOpenInNewFrame)?.also {
-      StartupManager.getInstance(it).runWhenProjectIsInitialized {
-        ZigProjectsService.guessAndSetupZigProject(it)
-      }
-    }
+    return PlatformProjectOpenProcessor.getInstance().doOpenProject(rootDirectory, projectToClose, forceOpenInNewFrame)
   }
 }
