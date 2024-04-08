2723d8452496f310a18542e5130993e606de9af5
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-04-08 15:26:02 +0800

    [zig] add project open processor

--- zig/zig-plugin/src/zigzen/openapi/components/AbstractZigProjectSettingsService.kt
+++ zig/zig-plugin/src/zigzen/openapi/components/AbstractZigProjectSettingsService.kt
@@ -1,11 +1,12 @@
 // Copyright 2024 ZigIDE and contributors. Use of this source code is governed by the Apache 2.0 license.
 package zigzen.openapi.components
 
+import com.intellij.openapi.components.BaseState
 import com.intellij.openapi.components.PersistentStateComponent
 import com.intellij.openapi.project.Project
 import com.intellij.util.xmlb.XmlSerializerUtil
 
-abstract class AbstractZigProjectSettingsService<T: Any>(
+abstract class AbstractZigProjectSettingsService<T: AbstractZigProjectSettingsService.AbstractZigProjectSettings<T>>(
   project: Project,
   private val initialState: T
 ) : PersistentStateComponent<T> {
@@ -13,5 +14,13 @@ abstract class AbstractZigProjectSettingsService<T: Any>(
 
   override fun loadState(state: T) {
     XmlSerializerUtil.copyBean(state, initialState)
+  }
+
+  fun modify(action: (T) -> Unit) {
+    state.also(action)
+  }
+
+  abstract class AbstractZigProjectSettings<T : AbstractZigProjectSettings<T>> : BaseState() {
+    abstract fun copy(): T
   }
 }
