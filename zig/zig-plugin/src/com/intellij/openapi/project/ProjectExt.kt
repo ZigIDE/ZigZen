c7d80a0e2afcc91a079cc7dc75afd87f29702a6c
HTGAzureX1212 <39023054+HTGAzureX1212@users.noreply.github.com>
2025-01-15 13:17:12 +0800

    [zig] project refresh use EDT and coroutine

--- zig/zig-plugin/src/com/intellij/openapi/project/ProjectExt.kt
+++ zig/zig-plugin/src/com/intellij/openapi/project/ProjectExt.kt
@@ -5,6 +5,7 @@ import zigzen.lang.toolchain.AbstractZigToolchain
 import zigzen.openapi.components.IZigProjectsService
 import zigzen.openapi.components.ZigProjectSettingsService
 import com.intellij.openapi.components.service
+import kotlinx.coroutines.CoroutineScope
 
 val Project.containsZigProjects: Boolean
   get() = !zigProjects.allProjects.isEmpty()
@@ -17,3 +18,6 @@ val Project.toolchain: AbstractZigToolchain?
 
 val Project.zigProjects: IZigProjectsService
   get() = service()
+
+val Project.coroutineScope: CoroutineScope
+  get() = zigProjects.coroutineScope
